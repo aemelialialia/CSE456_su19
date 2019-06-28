a89eb7e6f4af8403fa8cc414a644a64ff72b5522
Emily Zhang <aiwenz@cs.washington.edu>
2019-06-27 19:59:27 -0700

    new files added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 5;
+    public Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+        {
+            //move up
+            rigid.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            //move down
+            rigid.MovePosition(transform.position + (-transform.forward) * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.MovePosition(transform.position + (-transform.right) * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigid.MovePosition(transform.position + (transform.right) * speed * Time.deltaTime);
+        }
+	}
+}
