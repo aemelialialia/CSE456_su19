49d76a0c4c568c890c91cb175faa0ef1937ce2cd
Emily Zhang <emily.aiwen@gmail.com>
2019-07-01 20:58:32 -0700

    added files and folders

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 5;
+    public Rigidbody rigid;
+    public Animator anime;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        anime.SetBool("forward", false);
+        anime.SetBool("backward", false);
+		if (Input.GetKey(KeyCode.W))
+        {
+            //move up
+            rigid.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+            anime.SetBool("forward", true);
+            anime.SetBool("backward", false);
+       
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            //move down
+            rigid.MovePosition(transform.position + (-transform.forward) * speed * Time.deltaTime);
+            anime.SetBool("forward", false);
+            anime.SetBool("backward", true);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.MovePosition(transform.position + (-transform.right) * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigid.MovePosition(transform.position + (transform.right) * speed * Time.deltaTime);
+        }
+	}
+}
